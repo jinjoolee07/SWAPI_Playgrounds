0edeef22fbf90ae9fa973110a15c7c15b47d7c47
Jin Joo Lee <jinjoolee07@gmail.com>
2021-05-03 18:41:32 -0400

    swapi done

--- SWAPI.playground/Contents.swift
+++ SWAPI.playground/Contents.swift
@@ -1,1 +1,96 @@
-import Foundation
+import UIKit
+
+struct Person: Decodable {
+    let name: String
+    let films: [URL]
+}
+
+struct Film: Decodable {
+    let title: String
+    let opening_crawl: String
+    let release_date: String
+}
+
+class SwapiService {
+    
+    static private let baseURL = URL(string: "https://swapi.dev/api/")
+    static let peopleEndpoint = "people"
+    static let filmsEndpoint = "films"
+    
+    static func fetchPerson(id: Int, completion: @escaping (Person?) -> Void) {
+        // 1 - Prepare URL
+        guard let baseURL = baseURL else { return completion(nil)}
+        let peopleURL = baseURL.appendingPathComponent(peopleEndpoint)
+        let finalURL = peopleURL.appendingPathComponent("\(id)")
+        print(finalURL)
+        
+        // 2 - Contact server
+        URLSession.shared.dataTask(with: finalURL) { (data, _, error) in
+            // 3 - Handle errors
+            if let error = error {
+                print("Error in \(#function) : \(error.localizedDescription) \n---\n \(error)")
+                return completion(nil)
+            }
+            
+            // 4 - Check for data
+            guard let data = data else { return completion(nil)}
+            
+            // 5 - Decode Person from JSON
+            do {
+                let decoder = JSONDecoder()
+                let person = try decoder.decode(Person.self, from: data)
+                return completion(person)
+            } catch {
+                print("Error in \(#function) : \(error.localizedDescription) \n---\n \(error)")
+                return completion(nil)
+            }
+        }.resume()
+    }
+    
+    static func fetchFilm(url: URL, completion: @escaping (Film?) -> Void) {
+        
+        // 1 - Contact server
+        URLSession.shared.dataTask(with: url) { (data, _, error) in
+            
+            // 2 - Handle errors
+            if let error = error {
+                print("Error in \(#function) : \(error.localizedDescription) \n---\n \(error)")
+                return completion(nil)
+            }
+            
+            // 3 - Check for data
+            guard let data = data else { return completion(nil) }
+            
+            // 4 - Decode Film from JSON
+            do {
+                let decoder = JSONDecoder()
+                let film = try decoder.decode(Film.self, from: data)
+                completion(film)
+            } catch {
+                print("Error in \(#function) : \(error.localizedDescription) \n---\n \(error)")
+                return completion(nil)
+            }
+        }.resume()
+    }
+}
+
+func fetchFilm(url: URL) {
+    SwapiService.fetchFilm(url: url) { film in
+        if let film = film {
+            print(film.title)
+            print(film.opening_crawl)
+            print(film.release_date)
+        }
+    }
+}
+
+
+SwapiService.fetchPerson(id: 2) { person in
+    if let person = person {
+        print(person.name)
+        for film in person.films {
+            fetchFilm(url: film)
+        }
+    }
+}
+
